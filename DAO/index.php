003888f6edbb208c0414bc3ffc9cd41cf2468ea1
Andre Luiz Silva de Castro <andre-si2011@hotmail.com>
2022-06-01 22:07:15 -0400

    selcet funcionando

--- DAO/index.php
+++ DAO/index.php
@@ -1,8 +1,15 @@
 <?php
 
 require_once("config.php");
-$sql = new sql();
-$usuario = $sql -> select ("SELECT * FROM tb_usuario");
-echo json_encode($usuario);
+
+$root = new Usuario();
+
+$root->loadbyId(2);
+
+echo $root;
+
+//$sql = new sql();
+//$usuario = $sql -> select ("SELECT * FROM tb_usuario");
+//echo json_encode($usuario);
 
 ?>
